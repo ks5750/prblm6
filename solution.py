e5197c9969937ac5da26919e399ab2154791f9ff
kumar <kumar.saswat@gmail.com>
2021-12-09 12:11:48 -0500

    Initial commit

---
+++ solution.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python3
+
+import cryptography
+import nacl.secret
+from nacl.secret import SecretBox
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from nacl.public import PrivateKey, Box
+from nacl.public import PublicKey
+from nacl.secret import SecretBox
+from nacl.signing import SigningKey
+import sys
+import json
+from nacl.signing import VerifyKey
+
+
+#
+# with open(sys.argv[1]) as json_data:
+#     inputs = json.load(json_data)
+inputs = json.load(sys.stdin)
+outputs = {}
+
+# Problem 1
+input_1 = bytes.fromhex(inputs["problem1"])
+alice_public_key = PublicKey(input_1)  # their public key bytes go here
+
+BobsPrivateKey = PrivateKey(b"A" * 32)
+BobsPrivateKey.encode()
+nonce=b"B" * 24
+message = b"hello world"
+
+
+ciphertext_alice = Box(
+    BobsPrivateKey,
+    alice_public_key,
+).encrypt(message, nonce).ciphertext
+# print("alice encrypts:", ciphertext_alice.hex())
+outputs["problem1"] = ciphertext_alice.hex()
+
+
+
+# Problem 2
+input2_cypher= bytes.fromhex(inputs["problem2"])
+bob_public_key = PublicKey(input_1)  # their public key bytes go here
+
+privateK = PrivateKey(b"A" * 32)
+privateK.encode()
+nonce_2=b"C" * 24
+message = b"hello world"
+
+
+plantext_alice = Box(
+    privateK,
+    alice_public_key,
+).decrypt(input2_cypher, nonce_2)
+outputs["problem2"] = plantext_alice.decode()
+
+
+# Problem 2
+input2_cypher= bytes.fromhex(inputs["problem2"])
+bob_public_key = PublicKey(input_1)  # their public key bytes go here
+
+privateK = PrivateKey(b"A" * 32)
+privateK.encode()
+nonce_2=b"C" * 24
+message = b"hello world"
+
+
+plantext_alice = Box(
+    privateK,
+    alice_public_key,
+).decrypt(input2_cypher, nonce_2)
+outputs["problem2"] = plantext_alice.decode()
+
+# Problem 3
+
+outputs["problem3"] = Box(privateK, alice_public_key).encode().hex()
+
+
+# Problem 4
+input4_string= inputs["problem4"].encode()
+privKey4 = SigningKey(b"D" * 32)
+privKey4.encode()
+
+signature = privKey4.sign(input4_string).signature
+
+outputs["problem4"] = signature.hex()
+
+# Problem 5
+input5_array= inputs["problem5"]
+signing_public_key=bytes.fromhex(input5_array["signing_public_key"])
+signature=input5_array["signature"]
+candidates=input5_array["candidates"]
+
+their_public_key = VerifyKey(signing_public_key)
+their_public_key.encode()
+for x in candidates:
+    print(x)
+    try:
+        their_public_key.verify_key.verify(x.encode(),signature)
+    except Exception:
+
+        continue
+    outputs["problem5"] =x
+    break
+
+
+# Output
+#
+# In the video I wrote something more like `json.dump(outputs, sys.stdout)`.
+# Either way works. This way adds some indentation and a trailing newline,
+# which makes things look nicer in the terminal.
+print(json.dumps(outputs, indent="  "))
